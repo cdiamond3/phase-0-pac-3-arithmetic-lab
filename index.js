7eaaaac901b2744d5923f97be3f7339e04cb100f
cdiamond3 <diamond.chris.3@gmail.com>
2021-04-07 10:46:56 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(num1, num2) {
+    console.log(num1, num2)
+let res = num1 + num2
+    return res  
+}
+
+function subtract(num3, num4) {
+    let res = num3 - num4
+    return res
+}
+
+function multiply(num5, num6) {
+    let res = num5 * num6 
+    return res
+}
+
+function divide(num7, num8) {
+    let res = num7 / num8
+    return res
+}
+
+function increment(num1) {
+return ++num1;
+}
+
+function decrement(num1) {
+    return --num1;
+}
+
+function makeInt(num1) {
+return parseInt(num1, 10) 
+}
+
+function preserveDecimal(num1) {
+    return parseFloat(num1)
+}
+
